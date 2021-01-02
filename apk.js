393784009eecfd4d5b2094e5176eaa144598185c
test5729 <nitpar81@gmail.com>
2021-01-02 14:03:10 +0530

    add

---
+++ apk.js
@@ -0,0 +1,11 @@
+function create() {
+    this.add.circle(100,200,90,0xff0000)
+}
+
+const config = {
+    height: 500,
+    width: 250,
+    backgroundColor: "0x0000ff"
+}
+
+const game = new Phaser.Game(config);
